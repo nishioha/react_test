bedf98388de0d2d1c234c97fd6e8287a169a7ea5
nishioha <nishio0323@gmail.com>
2018-04-28 23:18:22 +0900

    クラス化

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const List = (props) => {
-  return (
-    <ul>
-      {props.items.map((item, i) => (
-        <li>{item}</li>
-      ))}
-    </ul>
-  );
+class List extends React.Component {
+  render(){
+    return (
+      <ul>
+        {this.props.items.map((item, i) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 ReactDOM.render(<List items={['hoge', 'huga', 'piyo']}/>, document.getElementById('root'));
