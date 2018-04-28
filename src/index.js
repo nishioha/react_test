c2dff74ead362c3feac4402d087b9d290f7c67e6
nishioha <nishio@nishioyoukanoMacBook-Air.local>
2018-04-28 22:21:45 +0900

    Listコンポーネントを作成

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const List = () => {
+  return (
+    <ul>
+      <li>hoge</li>
+      <li>huga</li>
+      <li>piyo</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(<List />, document.getElementById('root'));
 registerServiceWorker();
